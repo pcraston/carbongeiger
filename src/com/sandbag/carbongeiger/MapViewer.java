784259f769172122ff2e50ec17f4a999c29dca9f
Patrick Craston <patrick@craston.com>
2011-02-10 18:27:24 +0000

    use orientation sensor for beeping make beep work

--- src/com/sandbag/carbongeiger/MapViewer.java
+++ src/com/sandbag/carbongeiger/MapViewer.java
@@ -6,6 +6,10 @@ import java.util.Map;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -33,13 +37,16 @@ public class MapViewer extends MapActivity {
 	Location currentLocation;
     LocationManager locationManager;
 	LocationListener locationListener;
+	SensorManager mSensorManager;
+	Sensor mSensor;
 	String currentLat;
 	String currentLon;
 	MyLocationOverlay myLocationOverlay;
 	String nearestPolluter;
 	Location closestInstallation;
-	float orientation = -1;
-	float distance = 42000000;
+	float orientation = 0;
+	float difference = 0;
+	float distance;
 	MediaPlayer mp;
     
     /** Called when the activity is first created. */
@@ -49,37 +56,41 @@ public class MapViewer extends MapActivity {
         super.onCreate(savedInstanceState);
 		mp = MediaPlayer.create(this, R.raw.geigerclick);
     	initMap();        
-    	startGettingLocation();		
+//    	startGettingLocation();
+    	mSensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
     }
     
     /** Called when the activity is first returned to front. */
     @Override
-    public void onRestart()
+    public void onResume()
     {
-        super.onRestart();
-    	startGettingLocation();		
+        super.onResume();
+    	startGettingLocation();	
+    	mSensorManager.registerListener(mListener, mSensor, SensorManager.SENSOR_DELAY_GAME);
     }
     	
-    /** Called when the activity is stopped. */
-    @Override
-    protected void onPause() {
-        super.onPause();
-    	stopGettingLocation();
-    }
+//    /** Called when the activity is stopped. */
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//    	stopGettingLocation();
+//    }
     
     /** Called when the activity is stopped. */
     @Override
     protected void onStop() {
         super.onStop();
     	stopGettingLocation();
+    	mSensorManager.unregisterListener(mListener);
     }
     
-    /** Called when the activity is destroyed. */
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    	stopGettingLocation();
-    }
+//    /** Called when the activity is destroyed. */
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//    	stopGettingLocation();
+//    }
     
     public void initMap() {
         setContentView(R.layout.main);
@@ -90,63 +101,90 @@ public class MapViewer extends MapActivity {
 		myLocationOverlay = new MyLocationOverlay(this, mapView);
     }
     
+    private final SensorEventListener mListener = new SensorEventListener() {
+        public void onSensorChanged(SensorEvent event) {
+        	Log.d("carbongeiger","sensorChanged (new Azimuth " + event.values[0] + " east of magnetic north)");
+        	
+	    	if (closestInstallation != null) {
+				orientation = closestInstallation.bearingTo(currentLocation);
+//				this seems to be right, need to figure out why bearing seems to be counter-clockwise east of north
+				orientation = 360-orientation;
+//				Log.d("carbongeiger","Nearest polluter location: " + closestInstallation.toString());
+				Log.d("carbongeiger","orientation to nearest polluter: " + orientation);
+				difference = orientation - event.values[0];
+				difference = Math.abs(difference);
+				Log.d("carbongeiger","Difference between this and phone orientation: " + difference);
+	    		if (difference < 40) {
+	    			Log.d("carbongeiger","BEEP!");
+	    			mp.start();
+	    		}
+			} else {
+				Log.d("carbongeiger","no orientation to nearest polluter!");
+			}
+	
+//	    	if (currentLocation != null && closestInstallation != null) {
+//	    		float bearing = currentLocation.bearingTo(closestInstallation);
+//	    		if (bearing < 10 && bearing > 350) {
+//	    			Log.d("carbongeiger","BEEP!");
+//	//    			mp.start();
+//	    		}
+//	    	}
+        }
+
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
+
+    
+//    public void startGettingOrientation() {
+//    	sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    	
+
+//        myLocationOverlay.onSensorChanged(Sensor.TYPE_ORIENTATION, values)
+    		
+//        		Log.d("carbongeiger","myLocationOverlay: my orientation relative to north: " + myLocationOverlay.getOrientation());
+//        		Log.d("carbongeiger","location: my orientation relative to north: " + location.getBearing());
+//
+
+//    	  
+//    		public void onAccuracyChanged(int something, int accuracy) {
+//    		}
+//    	};
+//      sm.registerListener(sl, sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),sm.SENSOR_DELAY_GAME);    
+//    }
+    
+//    public void stopGettingOrientation() {	
+////    	sensorManager.unregisterListener(sensorEventListener);
+//    }
+    
     public void startGettingLocation(){
-        myLocationOverlay.enableCompass();
+//        myLocationOverlay.enableCompass();
         myLocationOverlay.enableMyLocation();
         mapView.getOverlays().add(myLocationOverlay);
         
-//        myLocationOverlay.onSensorChanged(Sensor.TYPE_ORIENTATION, values)
-        
-//    	SensorManager sm = (SensorManager) this.getSystemService(SENSOR_SERVICE);
-//    	SensorListener sl = new SensorListener() {
-//    		public void onSensorChanged(SensorEvent event) {
-//    			
-////    			if (currentLocation != null) {
-////	    			GeomagneticField geofield = new GeomagneticField((float) currentLocation.getLatitude(),
-////	    					(float) currentLocation.getLongitude(),
-////	    					(float) currentLocation.getAltitude(),
-////	    					currentLocation.getTime());
-////	    			float declination = geofield.getDeclination();    			
-////	    			Log.d("carbongeiger", "declination "+ declination);
-////    			}
-//    		}
-//    	  
-//    		public void onAccuracyChanged(Sensor sensor, int accuracy) {
-//    		}
-//    	};
         locationManager	= (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         locationListener = new LocationListener() {
             public void onLocationChanged(Location location) {
-
-                
-        		Log.d("carbongeiger","my orientation relative to north: " + myLocationOverlay.getOrientation());
-
-        		if (closestInstallation != null) {
-//        			what is the below giving me exactly? how calculate whether pointing towards?
-        			orientation = closestInstallation.bearingTo(currentLocation);
         		
-	    			Log.d("carbongeiger","orientation to nearest polluter: " + orientation);
-	        		if ((orientation > 0 && orientation < 30) || (orientation > 330 && orientation < 360)) {
-	        			Log.d("carbongeiger","BEEP!");
-//	        			mp.start();
-	        		}
-        		} else {
-        			Log.d("carbongeiger","no orientation to nearest polluter!");
-        		}
-        		
-            	if (currentLocation == null || location.distanceTo(currentLocation) > 100) {
+            	if (currentLocation == null || location.distanceTo(currentLocation) > 40000) {
+            		if (currentLocation == null) {
+//            			Log.d("carbongeiger","No currentLocation. Polling for first time...");
+            		} else if (closestInstallation == null ) {
+//            			Log.d("carbongeiger","No closestInstallation. Polling sandbag to get new markers...");            			
+            		} else {
+//            			Log.d("carbongeiger","Change in location is " + location.distanceTo(currentLocation) + ". Polling sandbag to get new markers...");
+            		}
                     mapController.animateTo(myLocationOverlay.getMyLocation());
 	            	currentLocation = location;
 	            	currentLat = Double.toString(location.getLatitude());
 	            	currentLon = Double.toString(location.getLongitude());
 	            	getMarkersForLocation();
+            	} else {
+//        			Log.d("carbongeiger","Change in location is " + location.distanceTo(currentLocation) + ". No need to poll right now...");            		
             	}
-//            	if (currentLocation != null && closestInstallation != null) {
-//            		float bearing = currentLocation.bearingTo(closestInstallation);
-//            		if (bearing < 10 && bearing > 350) {
-////            			mp.start();
-//            		}
-//            	}
+            	
+//            	Log.d("carbongeiger","location object says: " + location.toString());
+//            	Log.d("carbongeiger","MyLocationOverlay object says: " + myLocationOverlay.getMyLocation());
             }
 
             public void onStatusChanged(String provider, int status, Bundle extras) {}
@@ -159,7 +197,6 @@ public class MapViewer extends MapActivity {
         // Register the listener with the Location Manager to receive location updates
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-//        sm.registerListener(sl, sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),sm.SENSOR_DELAY_GAME);
     }
     
     public void stopGettingLocation(){
@@ -170,10 +207,11 @@ public class MapViewer extends MapActivity {
     }
     
     public void getMarkersForLocation() {
+    	distance = 42000000;
 		List<Overlay> mapOverlays = mapView.getOverlays();
 
 		String urltocall = "http://www.sandbag.org.uk/maps/installations_geiger/" + currentLat + '_' + currentLon + ".json";
-		Log.d("carbongeiger: ", "Calling URL " + urltocall);
+//		Log.d("carbongeiger", "Calling URL " + urltocall);
 	    MapDataService webService = new MapDataService(urltocall);
 	    //Pass the parameters if needed , if not then pass dummy one as follows
 	 	Map<String, String> params = new HashMap<String, String>();
@@ -198,9 +236,9 @@ public class MapViewer extends MapActivity {
 	 				nearestPolluter = current.name;
 	 		 		closestInstallation.setLatitude(current.lat);
 	 		 		closestInstallation.setLongitude(current.lon);
-//	 				Log.d("carbongeiger: ", "Current position: lat " + currentLat + " lon " + currentLon);
-//	 				Log.d("carbongeiger: ", nearestPolluter + " position: lat " + Double.toString(current.lat) + " lon " + Double.toString(current.lon));
-//	 				Log.d("carbongeiger: ", nearestPolluter + " distance: " + Double.toString(Math.floor(distance)));
+//	 				Log.d("carbongeiger", "Current position: lat " + currentLat + " lon " + currentLon);
+//	 				Log.d("carbongeiger", nearestPolluter + " position: lat " + Double.toString(current.lat) + " lon " + Double.toString(current.lon));
+//	 				Log.d("carbongeiger", nearestPolluter + " distance: " + Double.toString(Math.floor(distance)));
 	 			}
 	 			
 	 			GeoPoint point = new GeoPoint((int) (current.lat * 1E6), (int) (current.lon * 1E6));
@@ -220,7 +258,7 @@ public class MapViewer extends MapActivity {
 	 	}
 	 	catch(Exception e)
 	 	{
-//	 		Log.d("carbongeiger Error: ", e.getMessage());
+	 		Log.d("carbongeiger", "ERROR:" + e.getMessage());
 	 	}
     }
     
