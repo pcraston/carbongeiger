f7096d8d49d9bacbf3c31c289a969e8d1880f9d8
Patrick Craston <patrick@craston.com>
2011-04-27 15:47:55 +0000

    make app work on iphone

--- iphone/Classes/carbongeigerViewController.m
+++ iphone/Classes/carbongeigerViewController.m
@@ -40,8 +40,7 @@ BOOL firstpoll = TRUE;
 	locationManager.delegate = self; 
 	locationManager.desiredAccuracy = kCLLocationAccuracyBest; 
 	[locationManager startUpdatingLocation];
-	//currently deactivated because always on SanFran in simulator
-	mapView.showsUserLocation=NO;
+	mapView.showsUserLocation = YES;
 	mapView.mapType = MKMapTypeStandard;
 	[mapView setDelegate:self];
 	
@@ -90,7 +89,6 @@ BOOL firstpoll = TRUE;
 	[super viewDidUnload];
 }
 
-
 - (void)dealloc {
 	[locationManager release];
 	[motionManager stopDeviceMotionUpdates];
@@ -104,15 +102,17 @@ BOOL firstpoll = TRUE;
 //use accelerometer delegate to listen for changes in motion and update orientation and beep if required
 - (void) accelerometer:(UIAccelerometer *)accelerometer didAccelerate:(UIAcceleration *)acceleration {
 	// check that significant movement
-	if ((fabsf(acceleration.x) > 1.5 || fabsf(acceleration.y) > 1.5 || fabsf(acceleration.z) > 1.5) && nearestPolluterBearing && !firstpoll)  {
-		NSLog(@"%@", acceleration);
+	if ((fabsf(acceleration.x) > 1 || fabsf(acceleration.y) > 1 || fabsf(acceleration.z) > 1) && nearestPolluterBearing && !firstpoll)  {
+		//NSLog(@"x %f, x %f, x %f", acceleration.x, acceleration.y, acceleration.z);
 		//get orientation/attitude of phone from DeviceMotion
 		CMAttitude *attitude = motionManager.deviceMotion.attitude;
-		[attitude multiplyByInverseOfAttitude: referenceAttitude];
+		if (referenceAttitude != nil) [attitude multiplyByInverseOfAttitude: referenceAttitude];
 		float yaw = attitude.yaw * (180.0/M_PI);
 		float phonePolluterOrientation = nearestPolluterBearing - yaw;
 		phonePolluterOrientation = fabs(phonePolluterOrientation);
-		if (phonePolluterOrientation < 40 && nearestPolluterDistance < 5000) {
+        NSString *orientation = [NSString stringWithFormat: @"Phone-Polluter: %d",lroundf(phonePolluterOrientation)];
+        orientationLabel.text = orientation;
+		if ((phonePolluterOrientation < 40.0 || phonePolluterOrientation > 320.0) && nearestPolluterDistance < 5000.0) {
 			AudioServicesPlaySystemSound(soundFileObject);
 			AudioServicesPlaySystemSound (kSystemSoundID_Vibrate);
 		}
@@ -186,7 +186,7 @@ BOOL firstpoll = TRUE;
 	//convert to radians
 	nearestPolluterLat = nearestPolluterLat * (M_PI/180.0);
 	nearestPolluterLon = nearestPolluterLon * (M_PI/180.0);
-	//get orientation between phone and nearest polluter
+	//get orientation between current location and nearest polluter
 	float currentLat = (currentLocation.coordinate.latitude / 180.0) * M_PI;
 	float currentLon = (currentLocation.coordinate.longitude / 180.0) * M_PI;
 	nearestPolluterBearing = atan2(sin(nearestPolluterLon-currentLon)*cos(nearestPolluterLat), 
@@ -198,7 +198,6 @@ BOOL firstpoll = TRUE;
 	if (nearestPolluterBearing < 0) {
 		nearestPolluterBearing = 360 + nearestPolluterBearing;
 	}
-	NSLog(@"Closest polluter bearing: %f",nearestPolluterBearing);
 	
 	for (int i = 0; i < [installations count]; i++) {
 		NSDictionary *installation = [installations objectAtIndex:i];
@@ -217,6 +216,9 @@ BOOL firstpoll = TRUE;
 
 - (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id )annotation
 {
+    if ([annotation isKindOfClass:[MKUserLocation class]])
+        return nil;
+
 	MKAnnotationView *customAnnotationView = [[[MKAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:nil] autorelease];
 	UIImage *pinImage;
 	if ([annotation overalloc] == YES) {
@@ -249,7 +251,8 @@ BOOL firstpoll = TRUE;
 		}
 	}	
 	[customAnnotationView setImage:pinImage];
-	[pinImage release];
+	//do we need to release this? currently crashes if uncomment
+    //[pinImage release];
 	customAnnotationView.canShowCallout = YES;
 	return customAnnotationView;
 }
